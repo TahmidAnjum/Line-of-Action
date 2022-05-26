9976ae45fa2df1be6ed7613f85a2b9117980c526
TahmidAnjum <tahmidanjum320@gmail.com>
2022-05-26 11:52:24 +0600

    First Commit

--- experimental/script.sh
+++
@@ -1,6 +0,0 @@
-mkfifo pipe
-g++ one.cpp -o one
-python3 two.py < pipe | ./one | tee pipe
-#python3 two.py < pipe | ./one > pipe  # using this instead will not show output in the terminal
-#./one < pipe | python3 two.py | tee pipe  
-rm pipe
